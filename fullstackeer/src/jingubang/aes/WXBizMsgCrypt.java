d65cd0cf2de27be22826a0800d6af342036e5ab2
lihuadong <lihuadongv@163.com>
2020-02-16 14:07:58 +0800

    WXBizMsgCrypt

--- fullstackeer/src/jingubang/aes/WXBizMsgCrypt.java
+++ fullstackeer/src/jingubang/aes/WXBizMsgCrypt.java
@@ -253,38 +253,14 @@ public class WXBizMsgCrypt {
 		String signature = SHA1.getSHA1(token, timeStamp, nonce, encrypt[1].toString());
 
 		// 和URL中的签名比较是否相等
-		//System.out.println("第三方收到URL中的签名：" + msgSignature);
-		//System.out.println("本地校验的签名：" + signature);
+		// System.out.println("第三方收到URL中的签名：" + msg_sign);
+		// System.out.println("第三方校验签名：" + signature);
 		if (!signature.equals(msgSignature)) {
 			throw new AesException(AesException.ValidateSignatureError);
 		}
 
 		// 解密
 		String result = decrypt(encrypt[1].toString());
-		return result;
-	}
-	
-	//第三方平台收到的除了用户发的内容之外，微信自身提供的消息
-	public String decryptEvent(String msgSignature, String timeStamp, String nonce, String postData)
-			throws AesException {
-
-		// 密钥，公众账号的app secret
-		// 提取密文
-		Object[] encrypt = XMLParse.extract(postData,0);
-
-		// 验证安全签名
-		String signature = SHA1.getSHA1(token, timeStamp, nonce, encrypt[1].toString());
-
-		// 和URL中的签名比较是否相等
-		//System.out.println("decryptEvent():" + msgSignature);
-		//System.out.println("decryptEvent():" + signature);
-		if (!signature.equals(msgSignature)) {
-			throw new AesException(AesException.ValidateSignatureError);
-		}
-
-		// 解密
-		String result = decrypt(encrypt[1].toString());
-		//System.out.println("decryptEvent():" + result);
 		return result;
 	}
 
