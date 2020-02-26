c809f0c29bd921b4976241e655cc0578c144a38f
lihuadong <lihuadongv@163.com>
2020-02-26 23:44:52 +0800

    JSR 353 addresses the standard Java API for JSON processing and shipped as part of JEE 7.

--- fullstackeer/src/jingubang/json/JSONDemo.java
+++ fullstackeer/src/jingubang/json/JSONDemo.java
@@ -1,10 +1,52 @@
 package jingubang.json;
 
+
+import java.io.StringReader;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+
 public class JSONDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//String jsonStr = "{"access_token":"30_vxpDfBdeK3rc5pfAFYbVlti8Gl6wVkt8qKcmnbtixAXNEhRnofik6F8yQAIrpwwcVKdwwV1U_iLsabGniATuIAxTkyha6c9mNJCouvHUaLE5JmtAiMNMAK2eZnupt4iXES554ueB5ukB8MKaVOZfABAYRC","expires_in":7200}";
+	
+        String personJSONData = 
+                "  {" +
+                "   \"name\": \"Jack\", " +
+                "   \"age\" : 13, " +
+                "   \"isMarried\" : false, " +
+                "   \"address\": { " +
+                "     \"street\": \"#1234, Main Street\", " +
+                "     \"zipCode\": \"123456\" " +
+                "   }, " +
+                "   \"phoneNumbers\": [\"011-111-1111\", \"11-111-1111\"] " +
+                " }";
+             
+            JsonReader reader = Json.createReader(new StringReader(personJSONData));
+             
+            JsonObject personObject = reader.readObject();
+             
+            reader.close();
+             
+            System.out.println("Name   : " + personObject.getString("name"));
+            System.out.println("Age    : " + personObject.getInt("age"));
+            System.out.println("Married: " + personObject.getBoolean("isMarried"));
+
+            JsonObject addressObject = personObject.getJsonObject("address");
+            System.out.println("Address: ");
+            System.out.println(addressObject.getString("street"));
+            System.out.println(addressObject.getString("zipCode"));
+             
+            System.out.println("Phone  : ");
+             JsonArray phoneNumbersArray = personObject.getJsonArray("phoneNumbers");
+            for (JsonValue jsonValue : phoneNumbersArray) {
+                System.out.println(jsonValue.toString());
+            }
 	}
 
 }
