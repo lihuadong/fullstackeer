998b44e440dc709ff46b6a0efec93de331fd4884
lihuadong <lihuadongv@163.com>
2020-02-28 16:33:24 +0800

    JSON demo

--- fullstackeer/src/jingubang/json/JSONDemo.java
+++ fullstackeer/src/jingubang/json/JSONDemo.java
@@ -3,11 +3,11 @@ package jingubang.json;
 
 import java.io.StringReader;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.JsonValue;
+//import javax.json.Json;
+//import javax.json.JsonArray;
+//import javax.json.JsonObject;
+//import javax.json.JsonReader;
+//import javax.json.JsonValue;
 
 public class JSONDemo {
 
@@ -27,26 +27,26 @@ public class JSONDemo {
                 "   \"phoneNumbers\": [\"011-111-1111\", \"11-111-1111\"] " +
                 " }";
              
-            JsonReader reader = Json.createReader(new StringReader(personJSONData));
-             
-            JsonObject personObject = reader.readObject();
-             
-            reader.close();
-             
-            System.out.println("Name   : " + personObject.getString("name"));
-            System.out.println("Age    : " + personObject.getInt("age"));
-            System.out.println("Married: " + personObject.getBoolean("isMarried"));
-
-            JsonObject addressObject = personObject.getJsonObject("address");
-            System.out.println("Address: ");
-            System.out.println(addressObject.getString("street"));
-            System.out.println(addressObject.getString("zipCode"));
-             
-            System.out.println("Phone  : ");
-             JsonArray phoneNumbersArray = personObject.getJsonArray("phoneNumbers");
-            for (JsonValue jsonValue : phoneNumbersArray) {
-                System.out.println(jsonValue.toString());
-            }
+//            JsonReader reader = Json.createReader(new StringReader(personJSONData));
+//             
+//            JsonObject personObject = reader.readObject();
+//             
+//            reader.close();
+//             
+//            System.out.println("Name   : " + personObject.getString("name"));
+//            System.out.println("Age    : " + personObject.getInt("age"));
+//            System.out.println("Married: " + personObject.getBoolean("isMarried"));
+//
+//            JsonObject addressObject = personObject.getJsonObject("address");
+//            System.out.println("Address: ");
+//            System.out.println(addressObject.getString("street"));
+//            System.out.println(addressObject.getString("zipCode"));
+//             
+//            System.out.println("Phone  : ");
+//             JsonArray phoneNumbersArray = personObject.getJsonArray("phoneNumbers");
+//            for (JsonValue jsonValue : phoneNumbersArray) {
+//                System.out.println(jsonValue.toString());
+//            }
 	}
 
 }
